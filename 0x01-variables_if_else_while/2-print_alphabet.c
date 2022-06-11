ac187be09882749f59fc5454618a86e51bc50595
Your Erry77 <errystephen7@gmail.com>
2022-06-11 04:01:05 -0700

    alpa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random number to the variable n each
+ * and print the lowercase letters
+ * Return 0 Always return zero
+ */
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+
+return (0);
+}
