aecc511d93ad440d4fe194f3a1a6ced4620dc088
Your Erry77 <errystephen7@gmail.com>
2022-06-11 03:25:00 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * int main -Entry point of the program describing it as an interger function
+ * int main() -Entry point of the program describing it as an interger function
  * variable name is n
  *
  * the function int main represents intergers only
- * Always return zero at the end of the program
+ * Always return (0)zero at the end of the program
  */
 int main(void)
 {
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX /2; 
-printf("Last digit of %d is ",n);
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is ", n);
 if (n > 5)
 {
-	printf("greater than 5");
+printf("greater than 5");
 }
 if (n == 0)
 {
-	printf("greater than 5");
+printf("greater than 5");
 }
-if (n < 6 && n !=0)
+if (n < 6 && n != 0)
 {
-	printf("less than 6 not 0");
+printf("less than 6 not 0");
 }
 printf("\n");
 return (0);
