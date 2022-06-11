30934819a6005bf6598dbb386efe58d34093c8f3
Your Erry77 <errystephen7@gmail.com>
2022-06-11 05:54:05 -0700

    num

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,15 +4,15 @@
  * main - assign a random number to the variable n each
  * and print numbers from 1-10
  *
- * Return 0 Always return zero
+ * Retu5-print_numbers.crn 0 Always return zero
  */
 int main(void)
 {
 int n;
 for (n = 0; n <= 10; n++)
 {
-printf("%d\n", n);
+putchar("%d\n", n);
 }
 
-return (0);
+return (0) :
 }
