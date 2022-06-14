a8f5cdf6e7ab3ba00d182ebe2393403a96f5b705
Your Erry77 <errystephen7@gmail.com>
2022-06-14 08:53:15 -0700

    swap

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 
 /**
- * reset t0 98 > a function to update the reference of a pointer
+ * reset t0 98 -> a function to update the reference of a pointer
  * @n: pointer to n to be updated
  */
 void reset_to_98(int *n)
